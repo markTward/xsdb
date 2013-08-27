6505bd67db2f5e772db3e28d2d0b8af72ab12f06
Markosys MBPro <bitbucket@marktward.com>
2013-08-27 02:04:47 -0700

    improved rollbacks to only add only first key/value pair per level. introduced shlex for better simple string command line handling.  think i can leave it.  next example would be to make it class based.

--- dbxs.py
+++ dbxs.py
@@ -7,7 +7,7 @@ challenge to create an xtra small DB with set,get,unset and transaction scoping 
 
 '''
 
-import sys
+import sys, shlex
 
 db = {}
 idx = {}
@@ -20,21 +20,21 @@ def begin():
     global TRANLEVEL
     TRANLEVEL += 1
     if TRANLEVEL not in bi:
-        bi[TRANLEVEL] = []
+        bi[TRANLEVEL] = {}
     if DEBUG > 0: print 'BEGIN TRANLEVEL==>', TRANLEVEL, bi
     
 def rollback():
     global TRANLEVEL    
     if TRANLEVEL >= 1:
-        if DEBUG > 0: print 'REMOVE ROLLBACK TRANLEVEL==>', TRANLEVEL, bi[TRANLEVEL]
-        for trans in bi[TRANLEVEL]:
-            if DEBUG > 0: print '  BACKOUT transaction', trans
+        if DEBUG > 0: print 'ROLLBACK TRANLEVEL==>', TRANLEVEL, bi[TRANLEVEL]
+        for db_key ,rollback_value in bi[TRANLEVEL].items():
+            if DEBUG > 0: print '  BACKOUT transaction to', db_key ,rollback_value
             # rollback by executing previous state's set and unset commands
-            # is_rollback=True blocks rewrite to bi file
-            if trans[1] != None:
-                dbset(trans[0],trans[1], is_rollback=True)
+            # is_rollback=True blocks rewrite to bi file blocked
+            if rollback_value != None:
+                dbset(db_key, rollback_value, is_rollback=True)
             else:
-                dbunset(trans[0], is_rollback=True)
+                dbunset(db_key, is_rollback=True)
         # remove just restore bi level and decrement to previous transaction rollback level        
         bi.pop(TRANLEVEL)
         TRANLEVEL -= 1
@@ -56,11 +56,12 @@ def biwrite(k,v,is_rollback=False):
     # only write to bi-image at least one 'begin' has been invoked not a 'rollback'
     if TRANLEVEL < 1 or is_rollback: return
 
-    # TODO:
-    if k in db:
-        bi[TRANLEVEL].insert(0, (k,db[k]))
-    else:
-        bi[TRANLEVEL].insert(0, (k,None))   
+    # maintain state for k,v pair or k,None if it is new
+    if not k in bi[TRANLEVEL]:
+        if k in db:
+            bi[TRANLEVEL][k]=db[k]
+        else:
+            bi[TRANLEVEL][k]=None 
 
     if DEBUG > 0: print 'bi queue==>',bi
     
@@ -71,8 +72,8 @@ def idx_remove(k,v):
         idx.pop(k)
       
 def dbset(k,v,is_rollback=False):
-    # send tranactionto biwriter
-    # TODO: make biwrite into a decorator
+    # send transaction biwriter
+    # TODO: can biwrite be made into a decorator?
     biwrite(k,v,is_rollback)
         
     # if update and not create, remove previous index entry corresponding to previous value
@@ -87,7 +88,6 @@ def dbset(k,v,is_rollback=False):
         idx[v] = set()
         
     idx[v] = idx[v].union((k,))
-    
    
 def dbget(k):
     return db[k] if k in db else 'NULL'
@@ -96,10 +96,8 @@ def dbunset(k,is_rollback=False):
     if k in db:
         # send to bi writer; TODO: make into decorator?
         biwrite(k,db[k],is_rollback)
-        
         # remove index for value of k
         idx_remove(db[k],k)
-            
         # remove entry from database    
         db.pop(k)
 
@@ -108,36 +106,43 @@ def numequalto(k):
 
 def show():
     return 'db{}==>',db, 'idx{}==>', idx, 'bi{}==>',bi
-   
-def cmd_exec(cmd):
 
+def reset():
+    global db, idx, bi, TRANLEVEL
+    # reset all database, index, before-image and transaction level collection-globals
+    db, idx, bi, TRANLEVEL = {},{},{},0
+    
+def cmd_exec(cmd):
+    # assign functions to all available commands
+    cmdlist = {'set':dbset, 'unset':dbunset, 'get':dbget, 'numequalto':numequalto, 
+               'begin':begin,'rollback':rollback,'commit':commit, 
+               'show':show, 'reset':reset}
+    # commands producing output
+    printresult = ['get','numequalto','show', 'commit', 'rollback']
+    
     try:
-        icmd = cmd[0]        
+        assert cmd[0] in cmdlist    
     except:
-        print "cmd_exec() ERROR in retrieving command"
+        print "cmd_exec() ERROR: unknown command",cmd[0]
+        return
         
+    icmd = cmd[0]
     params = {}
     
+    # this only works on narrow spec, would need to push into command/argument validation
+    # TODO: better param handling
     if len(cmd) == 3:
         params = {'k':cmd[1],'v':cmd[2]}
     elif len(cmd) == 2:
         params = {'k':cmd[1]}
     
-    if DEBUG > 0: print 'input cmd_input::split==>', cmd, icmd, params
-    
-    cmdlist = {'set':dbset, 'unset':dbunset, 'get':dbget, 'numequalto':numequalto, 
-               'begin':begin,'rollback':rollback,'commit':commit, 
-               'show':show}
-    
-    printresult = ['get','numequalto','show', 'commit', 'rollback']
-    
+    # execute function defined in cmdlist with parameter blob
     try:
         result = cmdlist[icmd](**params)
         if icmd in printresult and result != None: print result
     except:
         print 'cmd_exec failed for:', icmd, params
      
-
 if __name__ == '__main__':
     while True: 
         try:
@@ -145,7 +150,9 @@ if __name__ == '__main__':
         except KeyboardInterrupt:
             break
         
+        # if END command or freestanding <RETURN>, exit
         if not cmd_input or cmd_input.lower() == 'end':
             break
           
-        cmd_exec(cmd_input.lower().split())    
+        # shlex enables simple multi-element key/value string handling for more interesting db
+        cmd_exec(shlex.split(cmd_input.lower()))    
